29bd10e12cd785f14993ebc6f4dfac1f5653ba8c
Leo <leo@marlevous.nl>
2024-10-07 16:13:18 +0200

    fixed the footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,12 @@ import Temperature from "@/components/Temperature";
 
 export default function Home() {
   return (
-    <div>
+    <div className="min-h-screen items-stretch border-8">
       <div className="p-8 flex flex-col md:flex-row gap-8 items-center sm:items-start">
         <Temperature />
         <LedSwitch />
       </div>
-      <div className="text-center p-4 mx-8 shadow-md border">
+      <div className="text-center fixed bottom-0 space-y-1 w-full mb-2">
         <p>Een project van Kees & Leo</p>
         <p>V0.5</p>
       </div>
